96ca7693dec773c302a876744cf435480c5edcc8
BBEAMss <thanasanbootrakane52@gmail.com>
2022-06-14 13:05:44 +0700

    modifile peerson

--- oop1/person.py
+++ oop1/person.py
@@ -1,5 +1,7 @@
 class person:
+    country = "Thaniland" #class variable
     def __init__(self,name,gender,profession,hours) -> None:
+        #instance variables
         self.name = name
         self.gender = gender
         self.profession = profession
@@ -27,3 +29,20 @@ jon.study()
 
 lalisa = person("Lalisa","Female","Korean Singer","13")
 lalisa.work()
+
+print(f"Class variable: {person.country}")
+print(f"Instance Variable: {lalisa.country}")
+
+#assing value
+lalisa.country = "Korea"
+print("------")
+print(f"Class variable: {person.country}")
+print(f"Instance Variable: {lalisa.country}")
+print(f"Instance Variable: {jon.country}")
+
+#assing calss varible
+person.country = "England"
+print("*******")
+print(f"Class variable: {person.country}")
+print(f"Instance Variable: {lalisa.country}")
+print(f"Instance Variable: {jon.country}")
