b6b15ba8c15a85708632c8dd11c312f9862dfafa
BBEAMss <thanasanbootrakane52@gmail.com>
2022-06-20 14:42:39 +0700

    module

---
+++ oop2/modulepackage/test_measure.py
@@ -0,0 +1,11 @@
+import measure_convert as mc
+
+if __name__ == '__main__':
+    print(mc.inch_to_cm(12.5)) # 12.5 นิ้ว = ? ซม
+    print(mc.cm_to_inch(100)) # 100 ซม = ? นิ้ว
+
+"""from measure_convert import inch_to_cm,cm_to_inch
+
+if __name__ == '__main__':
+    print(inch_to_cm(12.5)) 
+    print(cm_to_inch(100))"""
