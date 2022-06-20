dc3b08822e4d59df1c98d555528419928ad89113
BBEAMss <thanasanbootrakane52@gmail.com>
2022-06-20 15:41:46 +0700

    multiclass

---
+++ oop2/modulepackage/cafe.py
@@ -0,0 +1,17 @@
+class Dessert:
+    def __init__(self) -> None:
+        self.dessert = ['ice cream','sticky rice mango']
+
+    def show_dessert(self):
+        return f'Dessert: {self.dessert}'
+
+class Drink:
+    def __init__(self) -> None:
+        self.drink = ['coffe','tea','wine','soda']
+
+    def add_drink(self,new_drink:str):
+        #list.append(Value)
+        self.drink.append(new_drink)
+    
+    def show_drink(self):
+        return f'Drink: {self.drink}'
