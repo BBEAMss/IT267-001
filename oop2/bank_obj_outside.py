5aa91c61a85113584d0cad19656d74b07ee1f808
BBEAMss <thanasanbootrakane52@gmail.com>
2022-06-21 11:01:17 +0700

    package bank

---
+++ oop2/bank_obj_outside.py
@@ -0,0 +1,20 @@
+#this file is inside bank package
+#call module
+#from customer import Customer
+#from account import Account
+"""from bank import customer,account #วิทีที่1
+cus1 = customer.Customer()
+cus1_acc = account.Account()"""
+
+from bank.customer import Customer #วิทีที่2
+from bank.account import Account   #วิทีที่2
+
+cus1 = Customer()
+cus1.new_customer()
+
+cus1_acc = Account()
+cus1_acc.open_account(cus1.name,"Saving",'10-123-456',500)
+
+print("**** Open Bank Account Detai ****")
+print(cus1.cus_info())
+print(cus1_acc.display_balance())
